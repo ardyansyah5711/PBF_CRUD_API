cc6755f4ca0ccfb8b65b284e4c608d4b77c487ea
Ardyansyah Vira B <54975146+ardyansyah5711@users.noreply.github.com>
2021-04-30 22:54:05 +0700

    modul12

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCq1i3f4pC7QwP5uTFdO3SLVruNH66lFeE",
+  authDomain: "fir-login-f2506.firebaseapp.com",
+  projectId: "fir-login-f2506",
+  storageBucket: "fir-login-f2506.appspot.com",
+  messagingSenderId: "740032894175",
+  appId: "1:740032894175:web:7822365776712b0262456a",
+  measurementId: "G-F5VX66HWHJ"
+};
+  
+  export default firebaseConfig;
+  
